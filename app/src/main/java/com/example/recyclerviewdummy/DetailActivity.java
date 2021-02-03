e96166b68d19355faa0375f464ac3115c2ca53c8
IpMan522 <mihailanatolevic844@gmail.com>
2021-02-03 21:32:47 +0700

    Создал адаптер и настроил его работу и переход в детальную активность.

--- app/src/main/java/com/example/recyclerviewdummy/DetailActivity.java
+++ app/src/main/java/com/example/recyclerviewdummy/DetailActivity.java
@@ -2,13 +2,24 @@ package com.example.recyclerviewdummy;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class DetailActivity extends AppCompatActivity {
+
+    TextView details;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.detail_activity);
+
+        details = (TextView) findViewById(R.id.details);
+
+        Intent intent = getIntent();
+
+        String Details = intent.getStringExtra("Detailed");
+        details.setText(Details);
     }
 }
